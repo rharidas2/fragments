a79b726a3affb1924841c60ab6d47f6ee286fba2
Rohith Haridas <rharidas2@myseneca.ca>
2025-09-15 18:53:31 -0400

    Switched to src/index.js as main entry point and added dotenv

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// src/index.js
+
+// Load environment variables
+require('dotenv').config();
+
+// Logger setup
+const logger = require('./logger'); // make sure you have logger.js in src/
+
+// Handle crashes
+process.on('uncaughtException', (err, origin) => {
+  logger.fatal({ err, origin }, 'uncaughtException');
+  throw err;
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.fatal({ reason, promise }, 'unhandledRejection');
+  throw reason;
+});
+
+// Express setup
+const express = require('express');
+const app = express();
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
